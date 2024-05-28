7c0c4e572e4f5ea864c5e3d227a7c3b8433e6dd0
vasu <vasu@mindwaveventures.com>
2024-05-28 14:52:18 +0530

    App router configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import "./App.css";
-import { Button } from "./components/ui/button";
+import AppRoute from "./routes";
 
 function App() {
   return (
     <>
-      <h1>Home page</h1>
-      <Button>Click me</Button>
+      <AppRoute />
     </>
   );
 }
